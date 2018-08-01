7b748be04a060dfeb3bacf82f5c3067e5d573a98
Shogun222 <lionleo22222@gmail.com>
2018-08-01 16:28:12 -0700

    some new css and learnt javascript need to fix insta part and make second website

---
+++ app.js
@@ -0,0 +1,26 @@
+// console.log("app.js is sanity checked");
+//
+// $(document).ready(function(){
+//   console.log("jquery sanity checked")
+//
+//
+//
+// $(".jumbotron").click(function(){
+//     var colorArray = ["red", "green", "blue", "orange", "purple", "yellow"];
+//
+//     var rng = Math.random();
+//     rng = rng * 10 / 2;
+//     rng = Math.round(rng);
+//
+//     console.log(rng);
+//
+//     $(this).css("background-color", colorArray[rng] );
+// });
+
+
+
+
+
+
+
+});// NOTE: closes document.ready()
